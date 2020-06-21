f6abbcf293d4b84fe6c8853c3f85f714d89c152f
bkent <35678319+bkent@users.noreply.github.com>
2020-06-20 17:46:32 -0700

    Add T&C

---
+++ src/pages/terms-and-conditions.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../layouts/index'
+import Logo from '../assets/svg/KP_Thumbnail.svg'
+
+const TermsAndConditions = () => (
+
+    <Layout site="kindpost" headerClass="Header light">
+      <div className="contact-container">
+        <h1>Terms And Conditions</h1>
+        <Logo />
+      </div>
+    </Layout>
+
+
+)
+
+export default TermsAndConditions
