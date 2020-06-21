2787bce1d340c48c6c9c2cffd4e0b87650c6199d
bkent <35678319+bkent@users.noreply.github.com>
2020-06-20 17:44:20 -0700

    Add FAQ page

---
+++ src/pages/faq.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../layouts/index'
+import Logo from '../assets/svg/KP_Thumbnail.svg'
+
+const Faq = () => (
+
+    <Layout site="kindpost" headerClass="Header light">
+      <div className="contact-container">
+        <h1>FAQ</h1>
+        <Logo />
+      </div>
+    </Layout>
+
+
+)
+
+export default Faq
