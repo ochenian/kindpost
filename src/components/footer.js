7f40ce640d116f1d1fe3b78e41130fd5c1231b36
bkent <35678319+bkent@users.noreply.github.com>
2020-09-16 22:20:55 -0700

    Mobile styling for Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,40 +1,155 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { useMediaQuery } from 'react-responsive'
+import styled from 'styled-components'
+import TwitterLogo from '../assets/svg/Twitter_Logo_White.svg'
+import InstaLogo from '../assets/svg/insta_icon.svg'
+import FacebookLogo from '../assets/svg/facebook.svg'
 
-const Footer = () => (
-  <React.Fragment>
-    <div className="siteInfoContainer">
-      <div className="supportContainer">
-        <p>Info.</p>
+const MobileLinksSection = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin: 32px 0;
 
-        <Link to="/about">About us</Link>
-        <Link to="/faq">FAQ</Link>
-        <Link to="/contact">Contact us</Link>
+  &:first-of-type {
+    margin-top: 0;
+  }
+
+  &:last-of-type {
+    display: flex;
+    align-items: center;
+    margin-bottom: 0;
+  }
+`
+const MobileLink = styled(Link)`
+  padding: 8px;
+  border: 2px solid #fff;
+`
+
+const PrivacyText = styled.p`
+  font-size: 0.75rem;
+`
+
+const SectionHeader = styled.div`
+  font-size: 2rem;
+  margin-bottom: 16px;
+`
+
+const EmailContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 8px;
+
+  & input {
+    padding: 10px;
+    background: transparent;
+    border: none;
+    border-bottom: 1px solid #fff;
+    color: #fff;
+    flex: 1;
+  }
+
+  & button {
+    color: white;
+    text-transform: uppercase;
+    background: none;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    border-bottom: 1px solid #fff;
+    cursor: pointer;
+    letter-spacing: 0.1em;
+    font-family: 'Averia Serif Libre';
+  }
+`
+
+const StyledTwitterLogo = styled(TwitterLogo)`
+  width: 64px;
+  height: 64px;
+  cursor: pointer;
+`
+
+const StyledInstaLogo = styled(InstaLogo)`
+  width: 36px;
+  height: 36px;
+  fill: #fff;
+  cursor: pointer;
+`
+const StyledFacebookLogo = styled(FacebookLogo)`
+  width: 36px;
+  height: 36px;
+  fill: #fff;
+  cursor: pointer;
+`
+
+const Footer = () => {
+  const mobile = useMediaQuery({
+    query: '(max-width: 900px)'
+  })
+
+  return (
+    <React.Fragment>
+      <div className="siteInfoContainer">
+          {
+            !mobile &&
+              <>
+                <div className="links">
+                  <SectionHeader>Info.</SectionHeader>
+                  <Link to="/about">About us</Link>
+                  <Link to="/faq">FAQ</Link>
+                  <Link to="/contact">Contact us</Link>
+                </div>
+
+                <div className="links">
+                  <SectionHeader>Subscribe.</SectionHeader>
+                  <EmailContainer>
+                    <input />
+                    <button>Sign Up</button>
+                  </EmailContainer>
+
+                  <PrivacyText>YOUR EMAIL ADDRESS WILL BE USED IN ACCORDANCE WITH OUR PRIVACY POLICY</PrivacyText>
+                </div>
+
+                <div className="links">
+                  <SectionHeader>Follow.</SectionHeader>
+                  <div>Facebook</div>
+                  <div>Instagram</div>
+                  <div>Twitter</div>
+                </div>
+              </>
+          }
+
+          {
+            mobile &&
+              <>
+                <MobileLinksSection>
+                  <MobileLink to="/about">About us</MobileLink>
+                  <MobileLink to="/faq">FAQ</MobileLink>
+                  <MobileLink to="/contact">Contact us</MobileLink>
+                </MobileLinksSection>
+                <section>
+                  <EmailContainer>
+                    <input />
+                    <button>Sign Up</button>
+                  </EmailContainer>
+                </section>
+                <MobileLinksSection>
+                  <StyledInstaLogo />
+                  <StyledTwitterLogo />
+                  <StyledFacebookLogo />
+                </MobileLinksSection>
+              </>
+          }
       </div>
-      <div className="subscribeContainer">
-        <p>Subscribe.</p>
-        <div className="emailInput">
-          <input />
-          <button>Sign Up</button>
-        </div>
-
-        <p className="privacyText">YOUR EMAIL ADDRESS WILL BE USED IN ACCORDANCE WITH OUR PRIVACY POLICY</p>
-      </div>
-      <div className="followContainer">
-        <p>Follow.</p>
-        <div>Facebook</div>
-        <div>Instagram</div>
-        <div>Twitter</div>
-      </div>
-    </div>
-    {/* <div className="legal-footer">
-      <div>&copy; 2020 kindpost.</div>
-      <div>terms & conditions</div>
-      <div>privacy policy</div>
-    </div> */}
-  </React.Fragment>
-
-)
+      {/* <div className="legal-footer">
+        <div>&copy; 2020 kindpost.</div>
+        <div>terms & conditions</div>
+        <div>privacy policy</div>
+      </div> */}
+    </React.Fragment>
+  )
+}
 
 export default Footer
 
