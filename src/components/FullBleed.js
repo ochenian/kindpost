6e52b373f6d499c02a24207d2733eea7e7b87ecd
bkent <35678319+ochenian@users.noreply.github.com>
2020-12-13 20:43:29 -0800

    Footer mobile styling fixes; add mobile submit

--- src/components/FullBleed.js
+++ src/components/FullBleed.js
@@ -1,44 +1,45 @@
-import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import styled from 'styled-components'
-import CtaButton from './shared/Button'
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+import Link from 'gatsby-link';
+import CtaButton from './shared/Button';
 
 const Container = styled.div`
   width: 100vw;
   position: relative;
-  background: radial-gradient(transparent, rgba(0,0,0,0.5) 90%);
-`
+  background: radial-gradient(transparent, rgba(0, 0, 0, 0.5) 90%);
+`;
 
 const TextContainer = styled.div`
-    position: absolute;
-    color: #fff;
-    z-index: 10;
-    font-size: 2em;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
-    font-family: 'Averia Serif Libre';
-    font-weight: bold;
-    letter-spacing: 5px;
-    text-transform: lowercase;
-    text-shadow: 0 0 16px rgba(0,0,0,1);
-`
+  position: absolute;
+  color: #fff;
+  z-index: 10;
+  font-size: 2em;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  font-family: 'Averia Serif Libre';
+  font-weight: bold;
+  letter-spacing: 5px;
+  text-transform: lowercase;
+  text-shadow: 0 0 16px rgba(0, 0, 0, 1);
+`;
 
 const Text = styled.div`
   margin-bottom: 2rem;
   text-align: center;
-`
+`;
 
 const GrayscaleImg = styled(Img)`
   filter: brightness(60%) grayscale(0.5);
   height: 75vh;
   position: relative;
   z-index: -10;
-`
+`;
 
 const FullBleed = () => {
   const data = useStaticQuery(graphql`
@@ -53,17 +54,18 @@ const FullBleed = () => {
         }
       }
     }
-  `)
+  `);
   return (
     <Container>
       <TextContainer>
         <Text>sustainably sourced + carefully curated</Text>
-        <CtaButton>learn more</CtaButton>
+        <Link to="/about">
+          <CtaButton>learn more</CtaButton>
+        </Link>
       </TextContainer>
       <GrayscaleImg fluid={data.postcardSampleImg.childImageSharp.fluid} />
     </Container>
-  )
-}
+  );
+};
 
-
-export default FullBleed
+export default FullBleed;
