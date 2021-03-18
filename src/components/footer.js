62cb91238c7808d3df29e78f207c6ddb0cff618d
bkent <35678319+ochenian@users.noreply.github.com>
2021-03-17 17:08:39 -0700

    Rework footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,42 +1,102 @@
 import React, { useState } from 'react';
 import Link from 'gatsby-link';
+import Img from 'gatsby-image';
+import { useStaticQuery, graphql } from 'gatsby';
 import styled from 'styled-components';
 import addToMailchimp from 'gatsby-plugin-mailchimp';
 import { useMediaQuery } from '../hooks/useMediaQuery';
 import TwitterLogo from '../assets/svg/Twitter_Logo_White.svg';
 import InstaLogo from '../assets/svg/insta_icon.svg';
-import KpThumbnail from '../assets/svg/KP_Thumbnail.svg';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  // flex-wrap: wrap;
+  color: rgb(242, 235, 229);
+  font-family: 'Montserrat';
+  background: #282828;
+  // align-items: center;
+  padding: 3em;
+  text-transform: uppercase;
+
+  a {
+    color: rgb(242, 235, 229);
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 3em;
+`;
 
 const MobileLinksSection = styled.section`
   width: 100%;
   display: flex;
-  justify-content: space-between;
-  margin: 64px 0;
+  // justify-content: space-between;
+  // margin: 64px 0;
+  flex-direction: column;
+  text-transform: uppercase;
+  align-items: center;
+  margin: 3em 0;
 
   &:first-of-type {
-    margin-top: 0;
+    // margin-top: 0;
   }
 
   &:last-of-type {
     display: flex;
-    align-items: center;
+    // align-items: center;
     margin-bottom: 0;
     width: 50%;
+    flex-direction: row;
+    justify-content: space-evenly;
+    margin-top: 0;
   }
 `;
 const MobileLink = styled(Link)`
   padding: 8px;
-  width: 100px;
-  text-align: center;
+  // width: 100px;
+  // text-align: center;
+`;
+
+const LinksSectionWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const LinksSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 6em 3em 0;
+  font-size: 0.75rem;
+
+  a {
+    padding: 8px 0;
+  }
 `;
 
 const PrivacyText = styled.p`
-  font-size: 0.75rem;
+  font-size: 0.5rem;
 `;
 
 const SectionHeader = styled.div`
-  font-size: 2rem;
+  font-size: 1.75rem;
   margin-bottom: 16px;
+  text-transform: lowercase;
+  font-family: 'Averia Serif Libre';
+`;
+
+const SignUpDescription = styled.div`
+  text-transform: none;
+  margin-bottom: 1em;
+  font-style: italic;
+  font-size: 1rem;
 `;
 
 const EmailContainer = styled.div`
@@ -49,8 +109,8 @@ const EmailContainer = styled.div`
   & input {
     padding: 10px;
     background: transparent;
-    border: none;
-    border-bottom: 1px solid #fff;
+    border: 1px solid #fff;
+    border-right: none;
     color: #fff;
     flex: 1;
   }
@@ -59,13 +119,12 @@ const EmailContainer = styled.div`
     color: white;
     text-transform: uppercase;
     background: none;
-    border-top: none;
-    border-left: none;
-    border-right: none;
-    border-bottom: 1px solid #fff;
+    border: 1px solid #fff;
     cursor: pointer;
     letter-spacing: 0.1em;
-    font-family: 'Averia Serif Libre';
+    // font-family: 'Averia Serif Libre';
+    font-family: 'Montserrat';
+    padding: 11px;
   }
 `;
 
@@ -109,10 +168,17 @@ const LegalContainer = styled.div`
 
 const SignUpButton = styled.button`
   margin: 0;
+  padding: 11px;
+  cursor: pointer;
+
+  :hover {
+    background: #d4004c;
+    transition: all 0.5s;
+  }
 `;
 
 const Footer = () => {
-  const mobile = useMediaQuery('(max-width: 900px)');
+  const mobile = useMediaQuery('(max-width: 768px)');
 
   const [email, setEmail] = useState('');
   const [status, setStatus] = useState('');
@@ -132,21 +198,32 @@ const Footer = () => {
     setEmail(event.currentTarget.value);
   };
 
+  const data = useStaticQuery(graphql`
+    query Thumbnail {
+      kpThumbnail: file(relativePath: { eq: "KP_Thumbnail_Off_White.png" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          # Makes it trivial to update as your page's design changes.
+          fluid(maxWidth: 500, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <>
-      <div className="siteInfoContainer">
+      <Wrapper>
         {!mobile && (
           <>
-            <div className="links">
-              <SectionHeader>Info.</SectionHeader>
-              <a href="/about">About us</a>
-              <a href="/faq">FAQ</a>
-              <a href="/contact">Contact us</a>
-            </div>
-
-            <div className="links">
+            <Column>
               <form onSubmit={handleSubmit}>
-                <SectionHeader>Subscribe.</SectionHeader>
+                <SectionHeader>subscribe.</SectionHeader>
+                <SignUpDescription>
+                  Get uplifting stories and special offers delivered to your
+                  inbox.
+                </SignUpDescription>
                 {status.result !== 'success' && (
                   <EmailContainer>
                     <input
@@ -155,7 +232,7 @@ const Footer = () => {
                       aria-label="subscribe to email"
                       onChange={handleEmailChange}
                     />
-                    <button type="submit">Sign Up</button>
+                    <SignUpButton type="submit">join</SignUpButton>
                   </EmailContainer>
                 )}
                 {status.result === 'error' && <div>{status.msg}</div>}
@@ -175,34 +252,70 @@ const Footer = () => {
                   </a>
                 </PrivacyText>
               </form>
-            </div>
+              <div style={{ fontSize: '0.5rem', marginTop: 'auto' }}>
+                made with ♡ in california
+              </div>
+              <div style={{ fontSize: '0.5rem' }}>
+                &copy; {new Date().getFullYear()} kindpost llc. all rights
+                reserved.
+              </div>
+            </Column>
 
-            <div className="links">
-              <SectionHeader>Follow.</SectionHeader>
-              <a href="https://twitter.com/kindpostco">Twitter</a>
-              <a href="https://www.instagram.com/kindpostco">Instagram</a>
-            </div>
+            <Column>
+              <LinksSectionWrapper>
+                <LinksSection>
+                  <SectionHeader>Info.</SectionHeader>
+                  <a href="/about">About us</a>
+                  <a href="/faq">FAQ</a>
+                  <a href="/contact">Contact us</a>
+                </LinksSection>
+
+                <LinksSection>
+                  <SectionHeader>Follow.</SectionHeader>
+                  <a href="https://twitter.com/kindpostco">Twitter</a>
+                  <a href="https://www.instagram.com/kindpostco">Instagram</a>
+                </LinksSection>
+
+                <LinksSection>
+                  <SectionHeader>legal.</SectionHeader>
+                  <a href="/terms-of-service">terms of service</a>
+                  <a href="/privacy-policy">privacy policy</a>
+                </LinksSection>
+              </LinksSectionWrapper>
+            </Column>
+            <Column style={{ marginLeft: '-3em' }}>
+              <Img
+                fluid={data.kpThumbnail.childImageSharp.fluid}
+                style={{ width: '5vw', marginTop: '6px', maxWidth: '50px' }}
+              />
+            </Column>
           </>
         )}
 
         {mobile && (
           <>
-            <MobileLinksSection>
-              <MobileLink to="/about">About us</MobileLink>
-              <MobileLink to="/faq">FAQ</MobileLink>
-              <MobileLink to="/contact">Contact us</MobileLink>
-            </MobileLinksSection>
+            <Img
+              fluid={data.kpThumbnail.childImageSharp.fluid}
+              style={{ width: '8vw', marginBottom: '3em', maxWidth: '60px' }}
+            />
             <form onSubmit={handleSubmit}>
               <section>
                 {status.result !== 'success' && (
-                  <EmailContainer>
-                    <input
-                      name="email"
-                      type="text"
-                      onChange={handleEmailChange}
-                    />
-                    <SignUpButton type="submit">Sign Up</SignUpButton>
-                  </EmailContainer>
+                  <>
+                    {/* <SectionHeader>subscribe.</SectionHeader> */}
+                    <SignUpDescription>
+                      Get uplifting stories and special offers delivered to your
+                      inbox.
+                    </SignUpDescription>
+                    <EmailContainer>
+                      <input
+                        name="email"
+                        type="text"
+                        onChange={handleEmailChange}
+                      />
+                      <SignUpButton type="submit">join</SignUpButton>
+                    </EmailContainer>
+                  </>
                 )}
                 {status.result === 'error' && <div>{status.msg}</div>}
                 {status.result === 'success' && (
@@ -222,6 +335,13 @@ const Footer = () => {
               </PrivacyText>
             </form>
             <MobileLinksSection>
+              <MobileLink to="/faq">FAQ</MobileLink>
+              <MobileLink to="/about">About us</MobileLink>
+              <MobileLink to="/contact">Contact us</MobileLink>
+              <MobileLink to="/privacy-policy">privacy policy</MobileLink>
+              <MobileLink to="/terms-of-service">terms of service</MobileLink>
+            </MobileLinksSection>
+            <MobileLinksSection>
               <a href="https://twitter.com/kindpostco">
                 <StyledTwitterLogo />
               </a>
@@ -231,15 +351,15 @@ const Footer = () => {
             </MobileLinksSection>
           </>
         )}
-      </div>
-      <LegalContainer>
-        <div>
+      </Wrapper>
+      {/* <LegalContainer> */}
+      {/* <div>
           &copy; {new Date().getFullYear()} kindpost llc. all rights reserved.
         </div>
-        <div>made with ♡ in california</div>
-        <a href="/terms-of-service">terms of service</a>
-        <a href="/privacy-policy">privacy policy</a>
-        {!mobile && (
+        <div>made with ♡ in california</div> */}
+      {/* <a href="/terms-of-service">terms of service</a>
+        <a href="/privacy-policy">privacy policy</a> */}
+      {/* {!mobile && (
           <KpThumbnail
             style={{
               width: '24px',
@@ -247,8 +367,8 @@ const Footer = () => {
               marginLeft: 'auto',
             }}
           />
-        )}
-      </LegalContainer>
+        )} */}
+      {/* </LegalContainer> */}
     </>
   );
 };
