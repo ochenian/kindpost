839115fb9bf5fb5e93482205bf3de513416f467b
bkent <35678319+ochenian@users.noreply.github.com>
2021-03-25 20:36:46 -0700

    Hero styling & animation updates

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSpring, useTrail, animated, config } from 'react-spring';
 import { useStaticQuery, graphql, Link } from 'gatsby';
 import styled from 'styled-components';
 import { motion, useTransform, useViewportScroll } from 'framer-motion';
+import { Ease, gsap } from 'gsap';
+import 'splitting/dist/splitting.css';
+import 'splitting/dist/splitting-cells.css';
+import Splitting from 'splitting';
 import TwitterLogo from '../assets/svg/Twitter_Logo_White.svg';
 import InstaLogo from '../assets/svg/insta_icon.svg';
+import KpLogo from '../assets/svg/KP_Thumbnail_white.svg';
 import TextRotator from './TextRotaor';
 import CtaButton from './shared/Button';
 import { useMediaQuery } from '../hooks/useMediaQuery';
@@ -52,18 +57,61 @@ const StyledInstaLogo = styled(InstaLogo)`
 
 const Description = styled.div`
   letter-spacing: 2px;
-  width: 660px;
+  // width: 660px;
   font-size: 1.25rem;
   line-height: 1.5;
   margin-bottom: 2em;
 
+  div {
+    overflow: hidden;
+  }
+
   @media (max-width: 1024px) {
     font-size: 1rem;
-    width: 90vw;
+    // width: 90vw;
     margin: 2rem auto;
     letter-spacing: 1px;
     max-width: 50ch;
   }
+`;
+
+const Overlay = styled.div`
+  height: 100%;
+  background: rgb(191, 158, 154);
+  position: relative;
+  z-index: 100;
+  y: 0;
+  overflow: hidden;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledKpLogo = styled(KpLogo)`
+  width: 100%;
+  min-width: 25px;
+  max-width: 100px;
+  position: relative;
+`;
+
+const SendYourText = styled.div`
+  margin-right: 0.25em;
+  overflow: hidden;
+  text-shadow: 1px 1px 1px rgb(0 0 0 / 80%);
+`;
+
+const ANewWayText = styled.div`
+  overflow: hidden;
+  font-size: 2em;
+  letter-spacing: 0.18em;
+  margin-bottom: 16px;
+
+  @media (max-width: 600px) {
+    font-size: 24px;
+  }
+
+  margin-bottom: 0.5em;
 `;
 
 const Hero = () => {
@@ -71,6 +119,9 @@ const Hero = () => {
   const { scrollYProgress } = useViewportScroll();
   const y = useTransform(scrollYProgress, [0, 1], [0, 2000]);
   let trailConfig = { mass: 5, tension: 1000, friction: 100 };
+
+  const overlayRef = useRef();
+  const videoRef = useRef();
 
   const textSpring = useSpring({
     to: { opacity: 1 },
@@ -155,27 +206,122 @@ const Hero = () => {
   ];
   const clVideo = data.allCloudinaryMedia.edges[0];
 
+  const logoRef = useRef();
+  const rotatorTextRef = useRef();
+  const timelineSettings = {
+    staggerValue: 0.014,
+    charsDuration: 0.5,
+  };
+
+  useEffect(() => {
+    Splitting({ by: 'chars' });
+    const tl = gsap.timeline({
+      // scrollTrigger: {
+      //   trigger: overlayRef.current,
+      //   start: '1px',
+      // },
+    });
+    tl.from(logoRef.current, {
+      autoAlpha: 0,
+    })
+      .set('.line-1 .char, .large-text .char', {
+        y: '100%',
+      })
+      .set('.desc > div', { y: '100%', autoAlpha: 0 })
+      .set('.text-container a', { y: '100%', autoAlpha: 0 })
+      .set(rotatorTextRef.current, {
+        autoAlpha: 0,
+      })
+      // .to(overlayRef.current, {
+      //   scaleY: 0,
+      //   transformOrigin: 'top',
+      //   ease: 'power1.inOut',
+      //   delay: 0.5,
+      //   duration: 1.3,
+      // })
+      // .to(
+      //   logoRef.current,
+      //   {
+      //     // translateY: '500%',
+      //     // duration: 1,
+      //     autoAlpha: 0,
+      //   },
+      //   '-=1.5',
+      // )
+      // .fromTo(
+      //   videoRef.current,
+      //   {
+      //     scaleX: 1.2,
+      //     scaleY: 0.75,
+      //   },
+      //   {
+      //     scaleX: 1,
+      //     scaleY: 1,
+      //     transformOrigin: 'bottom',
+      //     duration: 1.3,
+      //     ease: 'power1.inOut',
+      //   },
+      //   '-=1.25',
+      // )
+      .to('.text-container', {
+        autoAlpha: 1,
+      })
+      .staggerTo(
+        '.line-1 .char, .large-text .char',
+        timelineSettings.charsDuration,
+        {
+          ease: 'Power3.easeOut',
+          y: '0%',
+        },
+        timelineSettings.staggerValue,
+        // '-=0.5',
+      )
+      .to(rotatorTextRef.current, { autoAlpha: 1 })
+      .to('.desc > div', {
+        ease: 'Power3.easeOut',
+        y: '0%',
+        autoAlpha: 1,
+      })
+      .to('.text-container a', {
+        ease: 'Power3.easeOut',
+        y: '0%',
+        autoAlpha: 1,
+      });
+    // .staggerTo(
+    //   '.desc .char',
+    //   timelineSettings.charsDuration,
+    //   {
+    //     ease: 'Power3.easeOut',
+    //     y: '0%',
+    //   },
+    //   timelineSettings.staggerValue,
+    // );
+  });
+
   return (
     <div className="Hero">
+      {/* <Overlay ref={overlayRef}>
+        <div ref={logoRef}>
+          <StyledKpLogo />
+        </div>
+      </Overlay> */}
       <div className="video-text">
         <div className="text-container">
-          <animated.div style={{ ...textSpring }} className="small-text">
+          <ANewWayText className="line-1" data-splitting>
             a new way to
-          </animated.div>
+          </ANewWayText>
           <div className="large-text">
-            {trail.map(({ opacity, ...rest }, index) => (
-              <animated.span key={index} style={{ ...rest, opacity }}>
-                {videoText[index]}
-              </animated.span>
-            ))}
-            {!mobile && (
-              <TextRotator
-                className="rotator"
-                content={rotatedText}
-                time={4000}
-                startDelay={1000}
-              />
-            )}
+            <SendYourText data-splitting>send your</SendYourText>
+            <div ref={rotatorTextRef}>
+              {!mobile && (
+                <TextRotator
+                  className="rotator"
+                  content={rotatedText}
+                  time={4000}
+                  startDelay={1000}
+                />
+              )}
+            </div>
           </div>
           {mobile && (
             <TextRotator
@@ -185,19 +331,22 @@ const Hero = () => {
               startDelay={1000}
             />
           )}
-          <Description>
-            Delivering inspirational and uplifting messages of positivity
-            handwritten on beautiful sustainably sourced vintage postcards. For
-            you or yours in kindness.
+          <Description className="desc">
+            <div>
+              Delivering inspirational and uplifting messages of positivity.
+            </div>
+            <div>
+              Handwritten on beautiful, sustainably-sourced, vintage postcards.
+            </div>
+            <div>For you or yours in kindness.</div>
           </Description>
 
           <a href="product">
             <CtaButton>send</CtaButton>
           </a>
         </div>
-        <motion.video
-          style={{ y, opacity: 0, scale: 0.75 }}
-          animate={{ opacity: 1, scale: 1 }}
+        <video
+          ref={videoRef}
           muted
           src={clVideo.node.secure_url}
           autoPlay
