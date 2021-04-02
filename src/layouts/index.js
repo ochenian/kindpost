cfdc1de49334b38cc82a6b9cdfcab1a1b19813e6
bkent <35678319+ochenian@users.noreply.github.com>
2021-04-01 21:11:40 -0700

    GDPR styling update

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,7 +30,6 @@ const Layout = ({ children, siteName, headerClass, location }) => {
         />
       </Helmet>
       <Cart />
-      {/* <Scroll /> */}
       <div className="Container">
         <button
           type="button"
@@ -49,49 +48,50 @@ const Layout = ({ children, siteName, headerClass, location }) => {
         >
           {children}
         </div>
-        {/* <Divider />
-        <How /> */}
+
         <CookieConsent
           overlay
           location="bottom"
           enableDeclineButton
           style={{
-            background: '#fff',
-            borderRadius: '4px',
-            color: '#000',
+            background: 'rgba(35, 34, 33, 0.95)',
+            // borderRadius: '4px',
+            color: '#fff',
             padding: mobile ? '24px' : '16px',
             alignItems: 'center',
             justifyContent: mobile ? 'center' : 'flex-start',
+            flexDirection: 'column',
+            width: mobile ? '100%' : '600px',
+            margin: '24px',
           }}
           contentStyle={{
             display: 'flex',
             margin: mobile ? '0' : '16px',
+            flex: '1',
           }}
-          buttonWrapperClasses={mobile ? 'cookie-button-wrapper' : ''}
-          buttonText="Accept Cookies"
+          buttonWrapperClasses={'cookie-button-wrapper'}
+          buttonText="ACCEPT"
           buttonStyle={{
             background: 'none',
-            border: 'solid #000 1px',
-            borderRadius: '4px',
-            color: '#000',
-            fontFamily: 'Averia Serif Libre',
+            border: 'solid #fff 1px',
+            // borderRadius: '4px',
+            color: '#fff',
+            letterSpacing: '2px',
+            // fontFamily: 'Averia Serif Libre',
             margin: mobile ? '24px 0' : '16px',
             padding: '16px 10px',
           }}
-          declineButtonText="Do Not Sell My Personal Information"
+          declineButtonText="DECLINE"
           declineButtonStyle={{
             background: 'none',
             textDecoration: 'underline',
-            color: '#000',
-            fontFamily: 'Averia Serif Libre',
-            margin: mobile ? '0' : '16px',
+            color: '#fff',
+            // fontFamily: 'Averia Serif Libre',
+            margin: '16px',
             padding: mobile ? '0' : '6px 10px',
           }}
           cookieName="gatsby-gdpr-google-analytics"
         >
-          {!mobile && (
-            <CookieSvg style={{ width: '48px', marginRight: '16px' }} />
-          )}
           This website uses cookies to enhance user experience and to analyze
           performance and traffic on our website. We also share information
           about your use of our site with our social media, advertising and
