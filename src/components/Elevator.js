c18ab9c975ddad8defc50e4e61ca05392adc4062
bkent <35678319+ochenian@users.noreply.github.com>
2021-05-31 21:44:14 -0700

    Responsive styling for Elevator

--- src/components/Elevator.js
+++ src/components/Elevator.js
@@ -9,7 +9,7 @@ const ElevatorWrapper = styled.section`
   align-items: center;
   flex-direction: column;
   padding: 96px 32px 48px;
-  width: 50ch;
+  max-width: 50ch;
   margin: 0 auto;
   color: #282828;
 `;
@@ -19,20 +19,33 @@ const SuperStatement = styled.p`
   font-weight: bold;
   color: #d4004c;
   margin-bottom: 8px;
+
+  @media (max-width: 900px) {
+    font-size: 0.75rem;
+  }
 `;
 
 const Statement = styled.p`
   font-weight: bold;
   font-size: 1.5rem;
+  text-align: center;
 
   &:last-of-type {
     margin-bottom: 16px;
+  }
+
+  @media (max-width: 900px) {
+    font-size: 1.25rem;
   }
 `;
 
 const Keywords = styled.div`
   font-size: 1rem;
   font-weight: bold;
+
+  @media (max-width: 900px) {
+    font-size: 0.75rem;
+  }
 `;
 
 const Divider = styled.span`
